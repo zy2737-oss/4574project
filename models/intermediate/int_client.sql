5be011bb20ba414578c30c2847d1598d0b4d83f7
Yijun Huang <yh3906@columbia.edu>
2026-04-21 03:43:14 +0000

    h

--- models/intermediate/int_client.sql
+++ models/intermediate/int_client.sql
@@ -1,10 +1,10 @@
 WITH temp_rank AS(
     SELECT *, ROW_NUMBER() OVER(PARTITION BY CLIENT_ID ORDER BY REQUEST_AT) row_n
-    FROM {f ref( 'base_box_requests') }}
+    FROM {{ ref( 'base_box__requests') }}
 )
 
 SELECT
-    CLIENT_ID
+    Client_ID,
     CLIENT ADDRESS,
     CLIENT_CITY,
     PHONE,
