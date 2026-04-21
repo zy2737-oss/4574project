db794da9862c297edf765fbb8937094cd5b60050
Zhiyong Lin <zl3602@columbia.edu>
2026-04-21 03:35:36 +0000

    add intermediate clean models

---
+++ models/intermediate/int_orders_clean.sql
@@ -0,0 +1,24 @@
+with orders as (
+
+    select *
+    from {{ ref('stg_orders') }}
+
+)
+
+select
+    fivetran_id,
+    order_id,
+    session_id,
+    client_name,
+    phone,
+    state,
+    shipping_address,
+    payment_method,
+    payment_info,
+    shipping_cost,
+    tax_rate,
+    order_at,
+    cast(order_at as date) as order_date,
+    fivetran_synced_at
+
+from orders
